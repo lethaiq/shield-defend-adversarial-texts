09fd12d2d4eaa7f4ddb488451782cdd1a0fc6f7e
lethaiq <leqthai.vn@gmail.com>
2022-04-08 00:30:04 -0400

    updates

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 import torch
+from sklearn.metrics import f1_score
+
 
 def evaluate_batch_single(model, batch, allow_grad=False, preds_only=False, device='cpu'):
     loss_func = torch.nn.CrossEntropyLoss()
@@ -21,3 +23,25 @@ def evaluate_batch(model, batch, allow_grad=False, ensemble_mean=True, train=Fal
     if not train:
         model.eval()
     return evaluate_batch_single(model, batch, allow_grad, preds_only=preds_only, device=device)
+
+
+def evaluate_without_attack(model, val_iter):
+    model.eval()
+    val_loss = []
+    preds = []
+    labels = []
+    val_acc = 0
+    total_val = 0
+
+    for batch in val_iter:
+        label = batch['labels'] if "bert" in str(model.name).lower() else batch.label
+        pred, loss, acc = evaluate_batch(model, batch)
+        val_loss.append(loss.item())
+        val_acc += acc
+        total_val +=len(batch['input_ids'])
+        preds.extend(pred.argmax(dim=-1).data.cpu().numpy())
+        labels.extend(label.data.cpu().numpy())
+    val_loss = np.mean(val_loss)
+    val_acc = val_acc/total_val
+    val_f1 = f1_score(labels, preds, average="weighted")  
+    return val_loss, val_acc, val_f1
