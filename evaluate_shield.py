7267718ff6778ab19a16ee3c443bc49dc2f85fe1
lethaiq <leqthai.vn@gmail.com>
2022-04-09 12:03:47 -0400

    updates

--- evaluate_shield.py
+++ evaluate_shield.py
@@ -12,7 +12,7 @@ max_len=32
 model_type = 'bert-base-uncased'
 dataset_name = 'subjectivity'
 device = 'cuda:0'
-attacker_name = 'TextBugger'
+attacker_name = 'DeepWordBug'
 inference_temp = 0.01
 rng = np.random.default_rng(12)
 
