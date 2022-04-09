54c69a1c3f888720916b63e2bf6c072b4b935150
lethaiq <leqthai.vn@gmail.com>
2022-04-09 11:07:29 -0400

    updates

--- model.py
+++ model.py
@@ -78,12 +78,13 @@ class BertClassifierDARTS(nn.Module):
                 self.darts_decision = nn.ParameterList([
                         torch.nn.Parameter(torch.randn(self.L)) for i in range(self.N)
                     ])
+
             else:
                 self.heads = nn.ModuleList([nn.ModuleList([nn.Sequential(nn.Linear(self.feature_dim, self.output_dim))]) for i in range(self.N)])
 
-            self.switch_train = torch.ones(self.N).to(self.device).float()
             self.switch = nn.Linear(self.N*self.output_dim+self.feature_dim, self.N)
-            self.probs = self.switch_train
+            self.probs = torch.ones(self.N).to(self.device).float()
+
         else:
             self.feature2main = nn.Linear(self.feature_dim, self.output_dim)
 
@@ -92,30 +93,31 @@ class BertClassifierDARTS(nn.Module):
     def select_head_gumbel_single(self, base_pred, pred_heads):
         pred_heads = torch.cat((pred_heads, base_pred),1)
         gumbel_t = self.switch(pred_heads).sum(0) # [N]
+
         if self.gumbel > 0:
             self.probs = F.softmax(self.add_gumbel(gumbel_t)*self.temperature, dim=-1)
             gumbel_t = gumbel_t * self.probs
+
         return gumbel_t
 
     def select_head_gumbel(self, base_pred, pred_heads):
         pred_heads = torch.cat(pred_heads,1) # batch size * (N * num_class)
         all_gumbel_t = []
+
         for j in range(len(pred_heads)):
             gumbel_t_single = self.select_head_gumbel_single(base_pred[j:j+1], pred_heads[j:j+1])
             all_gumbel_t.append(gumbel_t_single.unsqueeze(0))
         gumbel_t = torch.cat(all_gumbel_t, 0)
+
         return gumbel_t
 
     def forward_from_embedding(self, emb, attn_masks):
-        try:
-            cont_reps = self.bert_layer(inputs_embeds=emb, attention_mask=attn_masks)
-            pred = cont_reps.last_hidden_state[:, 0]
-        except:
-            cont_reps, _ = self.bert_layer(inputs_embeds=emb, attention_mask=attn_masks)
-            pred = cont_reps[:, 0]
-        pred = self.dropout(pred) if not self.inference else pred
+        cont_reps = self.bert_layer(inputs_embeds=emb, attention_mask=attn_masks)
+        pred = cont_reps.last_hidden_state[:, 0]
 
+        pred = self.dropout(pred) if not self.inference else pred
         pred = pred.view(-1, self.feature_dim)
+
         if self.ensemble > 0:
             self.pred_heads = []
             for i in range(len(self.heads)):
@@ -125,18 +127,21 @@ class BertClassifierDARTS(nn.Module):
                         controls = F.softmax(self.darts_decision[i], dim=-1)
                         scores = [(layer[j](pred)*controls[j]).unsqueeze(0) for j in range(len(layer))]
                         scores = torch.mean(torch.cat(scores, 0), 0)
+
                     else:
                         controls = F.softmax(self.darts_decision[i], dim=-1)
                         idx = torch.argmax(controls)
                         scores = layer[idx](pred)
                     scores = scores.view(-1, self.output_dim)
                     self.pred_heads.append(scores)
+
                 else:
                     self.pred_heads.append(layer[0](pred))
 
             self.random_key = self.select_head_gumbel(pred, self.pred_heads)
             pred = torch.cat([(self.pred_heads[i]*self.random_key[:,i].unsqueeze(1)).unsqueeze(1) for i in range(len(self.pred_heads))],1)
             pred = torch.mean(pred,1)
+
         else:
             key1 = self.feature2main(pred)
             pred = key1
@@ -146,8 +151,10 @@ class BertClassifierDARTS(nn.Module):
     def forward(self, seq, attn_masks, perturbation=[]):
         if len(perturbation) > 0:
             self.emb1 = Variable(self.embedding(seq)+perturbation, requires_grad=True)
+
         else:
             self.emb1 = Variable(self.embedding(seq), requires_grad=True)
+
         return self.forward_from_embedding(self.emb1, attn_masks)
 
     def add_gumbel(self, o_t, eps=1e-10):
@@ -156,6 +163,7 @@ class BertClassifierDARTS(nn.Module):
         u.uniform_(0, 1)
         g_t = -torch.log(-torch.log(u + eps) + eps)
         gumbel_t = o_t + g_t
+
         return gumbel_t
 
     def init_linear(self):  
