7700904d19f0f2e2caa6fe0fd0d09edac0a7e951
lethaiq <leqthai.vn@gmail.com>
2022-04-08 22:43:43 -0400

    updates

--- utils.py
+++ utils.py
@@ -5,18 +5,21 @@ from sklearn.metrics import f1_score
 
 def evaluate_batch_single(model, batch, allow_grad=False, preds_only=False, device='cpu'):
     loss_func = torch.nn.CrossEntropyLoss()
+    label = None
     with torch.set_grad_enabled(allow_grad):
         preds_prob = []
         seq = batch['input_ids']
         attn_masks = batch['attention_mask']
-        label = batch['labels']
+        if labels in batch:
+            label = batch['labels']
         preds = model(seq, attn_masks)
         if len(preds_prob) == 0:
             preds_prob = torch.nn.functional.softmax(preds, dim=-1)
         loss = None
         acc = None
         if not preds_only:
-            loss = loss_func(preds, label)
+            if label:
+                loss = loss_func(preds, label)
             acc = torch.sum(preds_prob.argmax(dim=-1) == label).item()
     return preds, loss, acc
 
@@ -30,7 +33,6 @@ def evaluate_without_attack(model, val_iter):
     model.eval()
     val_loss = []
     preds = []
-
     for batch in val_iter:
         pred, loss, acc = evaluate_batch(model, batch)
         val_loss.append(loss.item())
