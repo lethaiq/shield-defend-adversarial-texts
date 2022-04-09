c187c3f0c7c9145c8ae4be5919da0166618417a2
lethaiq <leqthai.vn@gmail.com>
2022-04-08 23:36:51 -0400

    updates

--- base.py
+++ base.py
@@ -17,7 +17,7 @@ save_path = './model.pt'
 patience = 2
 batch_size=128
 max_len=64
-dataset_name = 'spam'
+dataset_name = 'hatespeech'
 
 train_iter, val_iter, test_iter, tokenizer = prepare_dataset_bert('bert-base-uncased', 
                                                                 dataset_name, 
