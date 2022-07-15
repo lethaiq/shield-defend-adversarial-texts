71a10d3364f0f722d564fc6de1e84364b77ac2ff
lethaiq <leqthai.vn@gmail.com>
2022-07-15 10:44:46 -0400

    update

--- evaluate_base.py
+++ evaluate_base.py
@@ -1,6 +1,20 @@
 from datasets.utils.logging import set_verbosity_error
 set_verbosity_error()
 
+from contextlib import contextmanager
+
+@contextmanager
+def no_ssl_verify():
+    import ssl
+    from urllib import request
+
+    try:
+        request.urlopen.__kwdefaults__.update({'context': ssl.SSLContext()})
+        yield
+    finally:
+        request.urlopen.__kwdefaults__.update({'context': None})
+
+        
 from transformers import AutoTokenizer
 from model import *
 from dataset import *
